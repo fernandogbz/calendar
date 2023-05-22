b5826283be3f46e8dff2951d0d02f892dbfe74dc
Fernando Jimenez <fgjv06@gmail.com>
2023-05-21 22:39:23 -0400

    JS - getting new date, current year and month

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // Getting variables
 const currentDate = document.querySelector(".current-date");
 
+// getting new date, current year and month
+let date = new Date(),
+currentYear = date.getFullYear(),
+currentMonth = date.getMonth();

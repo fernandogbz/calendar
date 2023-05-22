8d97d73bd59cbafdef369e544d562c05b0a8c4b2
Fernando Jimenez <fgjv06@gmail.com>
2023-05-21 23:00:35 -0400

    JS - getting last date of the month

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ currentMonth = date.getMonth(); // getMonth() returns the month (0 to 11 of a da
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const renderCalendar = () => {
+  let lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // getting last date of the month
+  console.log(lastDateOfMonth);
   currentDate.innerText = `${months[currentMonth]} ${currentYear}`
 }
 

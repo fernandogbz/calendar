4ec7fc6cadd17f784ef3af84841b70584e963c0b
Fernando Jimenez <fgjv06@gmail.com>
2023-05-21 22:53:46 -0400

    JS - months array

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let date = new Date(),
 currentYear = date.getFullYear(),
 currentMonth = date.getMonth(); // getMonth() returns the month (0 to 11 of a date)
 
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 const renderCalendar = () => {
   currentDate.innerText = `${currentMonth} ${currentYear}`
 }
